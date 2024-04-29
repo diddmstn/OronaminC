6fad175c56f0358fb675f15bb2bc073435951826
양은수 <93758339+diddmstn@users.noreply.github.com>
2024-04-29 17:33:02 +0900

    Add classPlayer

---
+++ OronaminPC/OronaminPC/Player.cs
@@ -0,0 +1,25 @@
+﻿namespace OronaminPC
+{
+    internal class Player
+    {
+        public string name { get; set; }
+        public int level { get; set; }
+        public int attack { get; set; }
+        public int defense {  get; set; }
+        public int health { get; set; }
+        public int gold { get; set; }
+        
+        public Player(string _name,int _level, int _attack, int _defense, int _health, int _gold)
+        {
+            name = _name;
+            level = _level; 
+            attack = _attack;   
+            defense = _defense;
+            health = _health;
+            gold = _gold;   
+        }
+
+       
+      
+    }
+}
