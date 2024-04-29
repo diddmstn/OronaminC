aabd94368cc5f86832cafbd28b62eca9219a722d
Kwaksangwon93 <hs32456a@naver.com>
2024-04-29 16:24:20 +0900

    Add sin

---
+++ OronaminPC/OronaminPC/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OronaminPC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
