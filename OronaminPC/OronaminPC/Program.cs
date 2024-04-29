7db00c4515caff2120ab633bb9ac5ede07744b5b
Kwaksangwon93 <hs32456a@naver.com>
2024-04-29 18:11:41 +0900

    Add console

--- OronaminPC/OronaminPC/Program.cs
+++ OronaminPC/OronaminPC/Program.cs
@@ -4,7 +4,57 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("　　　　　　　　　　<<5로나민 PC방>>　　　　　　　　　　");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("　　　　　　　　　　　[ 메  뉴 ]　　　　　　　　　　　　");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("　1. 아이 찹다 아이스 커피 (상태 보기)　　　　　　　　　");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("　2. 아따 매버라 라볶이　(던전 입장)　　　　　　　　　　");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("　3. 저기요 여 에어컨 좀 틀아주소 (인벤토리)　　　　　　");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("　0. 워메 잼민이들때매 허파 뒤집히겄네  (나가기)　　　　");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("　무엇을 도와드릴까요 손님? ＼( ⌒  ∨ ⌒)／ 　　　　　　　");
+            Console.WriteLine("　>>");
+            Console.WriteLine("");
+            Console.WriteLine("▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.WriteLine("□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□");
+            Console.WriteLine("□□□■■■■□□□□■■■■■■□□■□□□□□■□□■■■■■□■□");
+            Console.WriteLine("□□■■□□■■□□□□□□□□■□□■□□□□□■□□■□□□■□■□");
+            Console.WriteLine("□□■■□□■■□□□■■■■■■□□■□□□□□■□□■□□□■□■□");
+            Console.WriteLine("□□□■■■■□□□□■□□□□□□□■□□□□□■■□■■■■■□■□");
+            Console.WriteLine("□□□□□□□□□□□■■■■■■□□■■■■■□■□□□□□□□□■□");
+            Console.WriteLine("□□□□■■□□□□□□□■■□□□□□□□□□□■□□■□□□□□□□");
+            Console.WriteLine("□■■■■■■■■□■■■■■■■■□□□□□□□■□□■■■■■■■□");
+            Console.WriteLine("□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□");
+            Console.WriteLine("□□□□□□□■■■■■□□□□□■■■■□□□□■□□□■□■□□□□");
+            Console.WriteLine("□□□□□□□■■□□■■□□□■■■■■■□□□■■■■■□■□□□□");
+            Console.WriteLine("□□□□□□□■■□□■■□□■■□□□□■■□□■□□□■□■■□□□");
+            Console.WriteLine("□□□□□□□■■□□■■□□■■□□□□□□□□■■■■■□■□□□□");
+            Console.WriteLine("□□□□□□□■■■■■■□□■■□□□□□□□□□□□□□□■□□□□");
+            Console.WriteLine("□□□□□□□■■□□□□□□■■□□□□■■□□□□■■■■□□□□□");
+            Console.WriteLine("□□□□□□□■■□□□□□□□■■■■■■□□□□■□□□□■□□□□");
+            Console.WriteLine("□□□□□□□■■□□□□□□□□■■■■□□□□□□■■■■□□□□□");
+            Console.WriteLine("□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□");
+            Thread.Sleep(50000);
         }
     }
 }
+
+
+
+
+
