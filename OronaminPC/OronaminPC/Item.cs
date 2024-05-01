96e012d7c0fc45c8814a2401f21b3adc7bb41178
Kwaksangwon93 <hs32456a@naver.com>
2024-05-01 12:50:54 +0900

    Add list

--- OronaminPC/OronaminPC/Item.cs
+++ OronaminPC/OronaminPC/Item.cs
@@ -20,9 +20,6 @@ namespace OronaminPC
         public bool IsEquip { get; private set; }
         public bool IsPurchase { get; private set; }
 
-        public List<Item> inventory;
-        public List<Item> store;
-
         public enum Type
         {
             마우스,
@@ -43,25 +40,40 @@ namespace OronaminPC
             type = _type;
         }
 
-        public void Init()
+        public List<Item> Init()
         {
-            inventory = new List<Item>();
-            store = new List<Item>();
+            List<Item> list = new List<Item>();
 
             Item mouse1 = new Item("볼 마우스", Type.마우스, "샷건치면 볼 사라짐", 700, 7, 0, 0, 0);
+            list.Add(mouse1);
             Item mouse2 = new Item("레이저 마우스", Type.마우스, "고양이 소환 가능", 1600, 14, 0, 0, 0);
+            list.Add(mouse2);
             Item mouse3 = new Item("게이밍 마우스", Type.마우스, "게임은 역시 장비빨", 4300, 28, 0, 0, 0);
+            list.Add(mouse3);
             Item keyboard1 = new Item("다이소 키보드", Type.키보드, "쒸프트 키까 안빠쪄요", 500, 0, 10, 0, 0);
+            list.Add(keyboard1);
             Item keyboard2 = new Item("사무용 키보드", Type.키보드, "일하기 싫을 때 흉기로", 1500, 0, 20, 0, 0);
+            list.Add(keyboard2);
             Item keyboard3 = new Item("게이밍 키보드", Type.키보드, "역시 돈도 재능이다", 4000, 0, 40, 0, 0);
+            list.Add(keyboard3);
             Item headset1 = new Item("고장난 헤드셋", Type.헤드셋, "한 쪽이 안들린다", 1000, 0, 0, 10, 0);
+            list.Add(headset1);
             Item headset2 = new Item("저급한 헤드셋", Type.헤드셋, "지지직은 BGM이겠지..", 2500, 0, 0, 20, 5);
+            list.Add(headset2);
             Item headset3 = new Item("게이밍 헤드셋", Type.헤드셋, "적의 숨소리가 들린다", 6000, 0, 0, 35, 15);
+            list.Add(headset3);
             Item drink1 = new Item("콜  라", Type.음료수, "PC방 콜라 국룰이지", 300, 0, 0, 30, 0);
+            list.Add(drink1);
             Item drink2 = new Item("핫식스", Type.음료수, "하루종일 게임 쌉가능", 500, 0, 0, 30, 10);
+            list.Add(drink2);
             Item drink3 = new Item("레드불", Type.음료수, "이거슨 합법적 각성제", 1500, 3, 5, 50, 15);
+            list.Add(drink3);
             Item drink4 = new Item("박카스", Type.음료수, "심장이 도킹도킹!!", 2500, 5, 10, 70, 20);
+            list.Add(drink4);
             Item drink5 = new Item("얼박사", Type.음료수, "맛있는데 힘까지 넘쳐", 4000, 10, 20, 100, 30);
+            list.Add(drink5);
+
+            return list;
         }
 
         internal void PrintItemInventory(bool withNumber = false, int idx = 0)
